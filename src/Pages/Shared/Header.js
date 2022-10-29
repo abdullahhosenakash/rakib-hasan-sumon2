7d615b140e44b99dd0cc5d41146d480ba1ca28da
abdullahhosenakash <abdullahhosenakash@gmail.com>
2022-10-29 10:27:59 +0600

    routes added

--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -6,42 +6,43 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../../Assets/logo2.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
 
 
 const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home"><img src={logo} alt="" srcset="" /></Navbar.Brand>
+                <Navbar.Brand as={Link} to='/'><img src={logo} alt="" srcSet="" /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
                         <NavDropdown title="Portfolio" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Street Photography</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
+                            <NavDropdown.Item as={Link} to='/streetPhotography'>Street Photography</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to='/documentaryPhotography'>
                                 Documentary Photography
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Travel Photography</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.4">
+                            <NavDropdown.Item as={Link} to='/travelPhotography'>Travel Photography</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to='/fineArtPhotography'>
                                 Fine Art Photography
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.4">
-                                Travel Photography                            </NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to='/naturePhotography'>
+                                Nature Photography                            </NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="#features">About</Nav.Link>
-                        <Nav.Link href="#features">Contact</Nav.Link>
+                        <Nav.Link as={Link} to='/about'>About</Nav.Link>
+                        <Nav.Link as={Link} to='/contact'>Contact</Nav.Link>
                     </Nav>
                     <Nav className="d-flex flex-row gap-3">
-                        <Nav.Link href="#deets">
+                        <Nav.Link href="https://www.facebook.com/rhsumonphoto" target="_blank">
                             <FontAwesomeIcon icon={faFacebookF} />
                         </Nav.Link>
-                        <Nav.Link href="#memes">
+                        <Nav.Link href="https://www.instagram.com/rhsumon90/" target="_blank">
                             <FontAwesomeIcon icon={faInstagram} />
                         </Nav.Link>
-                        <Nav.Link href="#memes">
+                        <Nav.Link href="https://twitter.com/rhsumonphoto" target="_blank">
                             <FontAwesomeIcon icon={faTwitter} />
                         </Nav.Link>
-                        <Nav.Link href="#memes">
+                        <Nav.Link href="https://www.eyeem.com/rhsumon" target="_blank">
                             <span>E</span>
                         </Nav.Link>
                     </Nav>
