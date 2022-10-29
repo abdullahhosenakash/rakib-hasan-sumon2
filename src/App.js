9bf983133eee3e96d00fb4c74a7500164b3d59e2
abdullahhosenakash <abdullahhosenakash@gmail.com>
2022-10-29 09:45:06 +0600

    router adder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Home from './Pages/Home/Home';
+import Header from './Pages/Shared/Header';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Home />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+
+      </Routes>
     </div>
   );
 }
